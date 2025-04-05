073cba0e59c02b23a65a76571058ef157bd1d249
victor88lm <victor88lm_@hotmail.com>
2025-04-05 12:53:16 -0600

    Seccion de inicio Completa

--- src/app/components/Inicio/home/home.component.ts
+++ src/app/components/Inicio/home/home.component.ts
@@ -20,65 +20,6 @@ export class HomeComponent implements OnInit {
   formError = false;
   errorMessage = '';
 
-  public cursos = [
-    {
-      id: 'dibujo-publicitario',
-      titulo: 'Dibujo Publicitario',
-      descripcion: 'Especialízate en técnicas de ilustración enfocadas a la publicidad y el marketing visual.',
-      imagen: 'img/dibujo-publicitario.jpg',
-      color: 'from-blue-500 to-indigo-600',
-      botonColor: 'from-blue-600 to-blue-500 hover:from-blue-700 hover:to-blue-600',
-      checkColor: 'text-blue-500',
-      caracteristicas: [
-        'Duración: 6 meses',
-        'Certificación oficial',
-        'Material incluido'
-      ]
-    },
-    {
-      id: 'diseno-grafico',
-      titulo: 'Diseño Gráfico',
-      descripcion: 'Aprende las herramientas digitales y principios de diseño para crear contenido visual impactante.',
-      imagen: 'img/diseno-grafico.jpg',
-      color: 'from-purple-500 to-pink-600',
-      botonColor: 'from-purple-600 to-purple-500 hover:from-purple-700 hover:to-purple-600',
-      checkColor: 'text-purple-500',
-      caracteristicas: [
-        'Duración: 8 meses',
-        'Software profesional',
-        'Portafolio final'
-      ]
-    },
-    {
-      id: 'talleres-especializados',
-      titulo: 'Talleres Especializados',
-      descripcion: 'Perfecciona técnicas específicas con nuestros talleres intensivos en diversas disciplinas artísticas.',
-      imagen: 'img/talleres-especializados.jpg',
-      color: 'from-green-500 to-teal-600',
-      botonColor: 'from-green-600 to-green-500 hover:from-green-700 hover:to-green-600',
-      checkColor: 'text-green-500',
-      caracteristicas: [
-        'Duración: Variable',
-        'Grupos reducidos',
-        'Instructores expertos'
-      ]
-    },
-    {
-      id: 'dibujo-infantil',
-      titulo: 'Dibujo Infantil',
-      descripcion: 'Desarrolla la creatividad y habilidades artísticas de los más pequeños con métodos pedagógicos adaptados.',
-      imagen: 'img/dibujo-infantil.jpg',
-      color: 'from-yellow-500 to-orange-600',
-      botonColor: 'from-yellow-600 to-yellow-500 hover:from-yellow-700 hover:to-yellow-600',
-      checkColor: 'text-yellow-500',
-      caracteristicas: [
-        'Edades: 5-12 años',
-        'Ambiente lúdico',
-        'Material incluido'
-      ]
-    }
-  ];
-
   contactForm: FormGroup;
   submitted = false;
   formSuccess = false;
