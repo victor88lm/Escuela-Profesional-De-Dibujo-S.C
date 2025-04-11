b57c72944de39af5b9ab31ef3b33253dd980f5ff
Oswaldo Benitez <oswaldobenitez34@gmail.com>
2025-04-10 23:28:43 -0600

    Actualizando talleres y cursos

--- src/app/components/cursos/pagos/pago-rechazado/pago-rechazado.component.ts
+++ src/app/components/cursos/pagos/pago-rechazado/pago-rechazado.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-pago-rechazado',
   templateUrl: './pago-rechazado.component.html',
-  styles: ``
+  standalone: true,
+  imports: [RouterModule],
 })
 export class PagoRechazadoComponent {
-
+  // Obtener el año actual para el pie de página
+  currentYear: number = new Date().getFullYear();
 }
